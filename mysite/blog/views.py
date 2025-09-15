65e2ae94378b76d6d76aac317c8ae3a805ec584d
OIITuMuCT <vataman@live.ru>
2025-09-15 20:52:09 +0300

    Sending emails with Django

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,23 +1,27 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
+from django.core.mail import send_mail
 from .models import Post
+from .forms import EmailPostForm
+
 
 class PostListView(ListView):
-    """ 
+    """
     Alternative post list view
     """
+
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    context_object_name = "posts"
     paginate_by = 2
-    template_name = 'blog/post/list.html'
+    template_name = "blog/post/list.html"
 
 
 def post_list(request):
     post_all = Post.published.all()
     # Pagination with 3 posts per page
     paginator = Paginator(post_all, 3)
-    page_number = request.GET.get('page', 1)
+    page_number = request.GET.get("page", 1)
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
@@ -25,7 +29,7 @@ def post_list(request):
     except EmptyPage:
         # If  page_number is out of range get last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {"posts": posts})
+    return render(request, "blog/post/list.html", {"posts": posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -35,6 +39,40 @@ def post_detail(request, year, month, day, post):
         slug=post,
         publish__year=year,
         publish__month=month,
-        publish__day=day
+        publish__day=day,
     )
-    return render(request, 'blog/post/detail.html', {'post': post})
+    return render(request, "blog/post/detail.html", {"post": post})
+
+
+def post_share(request, post_id):
+    """
+    Recommending posts by email
+    """
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (
+                f"{cd['name']} ({cd['email']}) " f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd["to"]],
+            )
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
