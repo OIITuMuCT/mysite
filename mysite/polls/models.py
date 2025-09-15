bcde61216c3a0e03fbd49d46a12366de6aed3a42
OIITuMuCT <vataman@live.ru>
2025-09-15 12:23:25 +0300

    Creating a Questionaire model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Questionnaire(models.Model):
+    """ Модель анкеты пользователя """
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    # question_list = models.ForeignKey('Question', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+# class Polls(models.Model):
+#     # question
+#     # answer
+#     pass
+
+# class Question(models.Model):
+#     # title
+#     # textfield
+#     # answer_option
+#     pass
+
+# class Answer(models.Model):
+#     #text choice
+#     pass
