f1e9422dedee194e0ee273e25e50afce4dcb6b12
OIITuMuCT <vataman@live.ru>
2025-09-17 23:30:33 +0300

    Addnin a form: SearchForm, a view: post_search

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,10 +4,15 @@ from django.views.generic import ListView
 from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
 from django.db.models import Count
-
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank
+)
 from taggit.models import Tag
+
 from .models import Post
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 
 
 class PostListView(ListView):
@@ -119,4 +124,31 @@ def post_comment(request, post_id):
         request,
         "blog/post/comment.html",
         {"post": post, "form": form, "comment": comment},
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body', config="russian")
+            search_query = SearchQuery(query, config='russian')
+            results = (Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+                )
+                .filter(search=query)
+                .order_by('-rank')
+            )
+    return render(
+        request, 'blog/post/search.html',
+        {
+            'form': form,
+            'query':query,
+            'results': results
+        }
     )
