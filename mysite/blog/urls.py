7afe035d56a18b0e28c9b3afcc867378143273a3
OIITuMuCT <vataman@live.ru>
2025-09-17 23:32:31 +0300

    Addnin a path: post_feed, post_search, post_list_by_tag

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = "blog"
 
 urlpatterns = [
     # Post views
     path("", views.post_list, name="post_list"),
-    path('tag/<slug:tag_slug>/', views.post_list, name="post_list_by_tag"),
+    path("tag/<slug:tag_slug>/", views.post_list, name="post_list_by_tag"),
     # path("", views.PostListView.as_view(), name="post_list"),
     path(
         "<int:year>/<int:month>/<int:day>/<slug:post>/",
@@ -16,4 +17,6 @@ urlpatterns = [
     ),
     path("<int:post_id>/share/", views.post_share, name="post_share"),
     path("<int:post_id>/comment/", views.post_comment, name="post_comment"),
+    path("feed/", LatestPostsFeed(), name="post_feed"),
+    path('search/', views.post_search, name='post_search'),
 ]
