0b20d6d6d1b161ae318dc20f29942e8088ad4b2d
OIITuMuCT <vataman@live.ru>
2025-09-24 21:30:34 +0300

    Adding URL edit

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    # path('login/', views.user_login, name='login'),
-    # login / logout urls
+    # # path('login/', views.user_login, name='login'),
+    # # login / logout urls
+
     # path("login/", auth_views.LoginView.as_view(), name="login"),
     # path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     # # change password urls
@@ -42,5 +43,5 @@ urlpatterns = [
     # dashboard
     path("", views.dashboard, name="dashboard"),
     path('register/', views.register, name='register'),
-    path('edit/', views.edit, name='edit')
+    path('edit/', views.edit, name='edit'),
 ]
