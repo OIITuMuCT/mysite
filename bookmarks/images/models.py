b4901bf962357b52f20f55781fab76427f664e0f
OIITuMuCT <vataman@live.ru>
2025-09-26 17:22:17 +0300

    Adding a get_absolute_url

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
@@ -34,3 +35,5 @@ class Image(models.Model):
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
