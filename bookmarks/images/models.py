7c6ae271ead5b1f35ed6f156fb648d3459bb7bb4
OIITuMuCT <vataman@live.ru>
2025-10-29 20:48:06 +0300

    Adding a ImageField, default file

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -12,7 +12,7 @@ class Image(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField(max_length=2000)
-    image = models.ImageField(upload_to='images/%Y/%m/%d')
+    image = models.ImageField(upload_to='images/%Y/%m/%d', default='images/no_image.jpg')
     description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
     users_like = models.ManyToManyField(
