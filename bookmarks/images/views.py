e4b63b99dd9a443a97ce8a050ccf76b34b718263
OIITuMuCT <vataman@live.ru>
2025-10-30 19:31:04 +0300

    Adding a create_action

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import ImageCreateForm
 from .models import Image
+from actions.utils import create_action
 
 @login_required
 def image_create(request):
@@ -16,6 +17,7 @@ def image_create(request):
             new_image = form.save(commit=False)
             new_image.user = request.user
             new_image.save()
+            create_action(request.user, 'bookmarked image', new_image)
             messages.success(
                 request, 'Image added successfully'
             )
@@ -46,6 +48,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action =='like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status':'ok'})
