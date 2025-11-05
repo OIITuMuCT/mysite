1b5d12f82f19d35ddeee528b598d35d91d62283d
OIITuMuCT <vataman@live.ru>
2025-11-05 22:13:33 +0300

    Adding a dashboard actions

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -15,12 +15,31 @@ from .forms import (
 from .models import Contact, Profile
 from .forms import LoginForm, UserRegistrationForm
 from actions.utils import create_action
+from actions.models import Action
 
 
 @login_required
 def dashboard(request):
     """ Dashboard """
-    return render(request, "account/dashboard.html", {"section": "dashboard"})
+    # Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list(
+        'id', flat=True
+    )
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    # actions = actions.select_related(
+    #     'user', 'user__profile'
+    # )[:10]
+    actions = actions.prefetch_related(
+        'target'
+    )[:10]
+    return render(
+        request,
+        "account/dashboard.html", 
+        {"section": "dashboard", 'actions': actions}
+    )
 
 
 def user_login(request):
