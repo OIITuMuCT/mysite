9e012d988f6a9f412cffbf39f02598cb15946e8c
OIITuMuCT <vataman@live.ru>
2025-11-05 22:14:13 +0300

    Install debug toolbar

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -40,14 +40,17 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "easy_thumbnails",
     "social_django",
     "django_extensions",
     "images.apps.ImagesConfig",
     "actions.apps.ActionsConfig",
+    
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -165,3 +168,16 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
 }
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
+REDIS_DB = 0
+
+if DEBUG:
+    import mimetypes
+    mimetypes.add_type('application/javascript', '.js', True)
+    mimetypes.add_type('text/css', '.css', True)
