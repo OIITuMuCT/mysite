70915255ca7b9f9a68b62a676d20670508263c9f
OIITuMuCT <vataman@live.ru>
2025-11-20 15:52:51 +0300

    Creating a view: CourseListView

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,8 +1,10 @@
+from django.db.models import Count
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from django.apps import apps
 from django.forms.models import modelform_factory
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic.base import TemplateResponseMixin, View
+from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
@@ -10,7 +12,7 @@ from django.contrib.auth.mixins import (
 )
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
 
 class OwnerMixin:
@@ -38,6 +40,29 @@ class ManageCourseListView(OwnerCourseMixin, ListView):
     # def get_queryset(self):
     #     qs = super().get_queryset()
     #     return qs.filter(owner=self.request.user)
+
+class CouseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(
+            total_modules=Count('courses')
+        )
+        courses = Course.objects.annotate(
+            total_modules=Count('modules')
+        )
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response(
+            {
+                'subjects': subjects,
+                'subject': subject,
+                'courses': courses
+            }
+        )
+
 
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
     permission_required = 'courses.add_course'
