65489d4fca0747f4b604fb90d48e1e004280ac6b
OIITuMuCT <vataman@live.ru>
2025-11-20 19:46:04 +0300

    Displaying the catalog of courses

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -14,6 +14,7 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
+from students.forms import CourseEnrollForm
 
 class OwnerMixin:
     def get_queryset(self):
@@ -66,7 +67,11 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
-    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["enroll_form"] = CourseEnrollForm(initial={"course": self.object})
+        return context
 
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
     permission_required = 'courses.add_course'
